31cfcfa9355453f65fd1a2a6d19aadf86d9dbec6
Masanori Ishiwata <mishiwata1015@gmail.com>
2021-05-27 11:28:07 +0900

    Add kana switcher to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,8 +3,6 @@
 hs.window.animationDuration = 0
 
 require("app_launcher")
--- require("global_keymap")
--- require("windows_manipulation")
--- require("config_reloading")
+require("kana_switcher")
 
 hs.alert.show("Config loaded")
