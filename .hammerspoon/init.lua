ad33d70c0268f69ba9b925f84b1e7dc4b4707f7c
Masanori Ishiwata <mishiwata1015@gmail.com>
2023-02-27 12:19:05 +0900

    Install MouseFollowsFocus

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -5,4 +5,8 @@ hs.window.animationDuration = 0
 require("app_launcher")
 require("kana_switcher")
 
+
+mouseFollowsFocus = hs.loadSpoon("MouseFollowsFocus")
+mouseFollowsFocus:start()
+
 hs.alert.show("Config loaded")
