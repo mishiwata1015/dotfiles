91b331d23af10fc318d3a3a68c81dd0af462e443
Masanori Ishiwata <mishiwata1015@gmail.com>
2026-03-15 23:35:25 +0900

    Remove redundant xcode-select check and unused log functions

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Xcode Command Line Tools
-if ! xcode-select -p &>/dev/null; then
-  xcode-select --install
-fi
-
 # Brew
 if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
