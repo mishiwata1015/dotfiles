4027248dd4936e0a2f814f10b723c4f6d94da263
Masanori Ishiwata <mishiwata1015@gmail.com>
2026-03-15 22:42:26 +0900

    Refactor kana_switcher: use lookup table and idiomatic Lua

--- .hammerspoon/kana_switcher.lua
+++ .hammerspoon/kana_switcher.lua
@@ -1,21 +1,21 @@
 local simpleCmd = false
-local map = hs.keycodes.map
+
+local keyActions = {
+    [hs.keycodes.map['cmd']]      = function() hs.keycodes.setLayout('ABC') end,
+    [hs.keycodes.map['rightcmd']] = function() hs.keycodes.setMethod('Hiragana') end,
+}
 
 local function kanaSwitchEvent(event)
-    local c = event:getKeyCode()
-    local f = event:getFlags()
-    if event:getType() == hs.eventtap.event.types.keyDown then
-        if f['cmd'] then
+    local eventType = event:getType()
+    if eventType == hs.eventtap.event.types.keyDown then
+        if event:getFlags()['cmd'] then
             simpleCmd = true
         end
-    elseif event:getType() == hs.eventtap.event.types.flagsChanged then
-        if not f['cmd'] then
-            if simpleCmd == false then
-                if c == map['cmd'] then
-                    hs.keycodes.setLayout('ABC')
-                elseif c == map['rightcmd'] then
-                    hs.keycodes.setMethod('Hiragana')
-                end
+    elseif eventType == hs.eventtap.event.types.flagsChanged then
+        if not event:getFlags()['cmd'] then
+            if not simpleCmd then
+                local action = keyActions[event:getKeyCode()]
+                if action then action() end
             end
             simpleCmd = false
         end
@@ -23,7 +23,7 @@ local function kanaSwitchEvent(event)
 end
 
 kanaSwitcher = hs.eventtap.new(
-  {hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged},
-  kanaSwitchEvent
+    {hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged},
+    kanaSwitchEvent
 )
 kanaSwitcher:start()
