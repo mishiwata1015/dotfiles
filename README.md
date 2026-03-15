779aedae2e7a3f89294b9bb49080654ae35dd94e
Masanori Ishiwata <mishiwata1015@gmail.com>
2026-03-15 22:52:52 +0900

    Improve init scripts and README: fix typos, robustness, add SSH note

--- README.md
+++ README.md
@@ -3,6 +3,14 @@ My dotfiles.
 
 ## Installation
 
+Before running, make sure your SSH key is set up for GitHub.
+
 ```bash
 $ bash -c "$(curl -fsSL https://raw.githubusercontent.com/mishiwata1015/dotfiles/main/etc/install)"
+```
+
+For work Mac, also run:
+
+```bash
+$ brew bundle --file=~/src/github.com/mishiwata1015/dotfiles/etc/init/Brewfile.work
 ```
--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Utilities
-is_exists() {
-  command -v "$1" > /dev/null 2>&1
-  return $?
-}
+# Xcode Command Line Tools
+if ! xcode-select -p &>/dev/null; then
+  xcode-select --install
+fi
 
 # Brew
-if ! is_exists "brew"; then
-  xcode-select --install
+if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
@@ -20,13 +18,10 @@ brew bundle --verbose --file="$DOTFILES_ROOT/etc/init/Brewfile"
 # vscode
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-rm "$VSCODE_SETTING_DIR/settings.json"
-ln -s "$DOTFILES_ROOT/.myapp/vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -sfn "$DOTFILES_ROOT/.myapp/vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -sfn "$DOTFILES_ROOT/.myapp/vscode/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
-rm "$VSCODE_SETTING_DIR/keybindings.json"
-ln -s "$DOTFILES_ROOT/.myapp/vscode/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
-
-cat "$DOTFILES_ROOT/.myapp/vscode/extensions.txt" | while read line
-do
- code --install-extension $line
-done
+while IFS= read -r line || [[ -n "$line" ]]; do
+  [[ -z "$line" ]] && continue
+  code --install-extension "$line"
+done < "$DOTFILES_ROOT/.myapp/vscode/extensions.txt"
